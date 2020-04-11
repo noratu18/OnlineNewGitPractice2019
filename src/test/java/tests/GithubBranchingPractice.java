de3e8d2a47dcb48e21db763038d1b085d2321d80
noratu18 <noratukesh@gmail.com>
2020-04-11 00:42:13 -0400

    branch practice added

---
+++ src/test/java/tests/GithubBranchingPractice.java
@@ -0,0 +1,11 @@
+package tests;
+
+public class GithubBranchingPractice {
+
+    // this just for practicing github branching:
+    // git branch name
+    // git checkout branch name --> to switch the brances
+    //git push —set-upstream origin set —>
+    // to create a remote branch on GitHub that corresponds to your newly created local branch.
+
+}
